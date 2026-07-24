6aba32ac2883e744b31eb6f2436cbebad4f67db0
qingmiz <qingmizoriginal@gmail.com>
2026-07-24 06:13:10 -0400

    update layout.tsx

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/shop',
-        permanent: true, // Set to true for permanent 301 redirect
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
